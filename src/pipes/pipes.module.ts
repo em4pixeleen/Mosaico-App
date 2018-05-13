11afdc8858fe2045a028fdad12a236bc62ef70a6
Daniel Verde <danielsanchezverde@me.com>
2018-05-13 15:23:16 +0200

    w

--- src/pipes/pipes.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { VideosPipe } from './videos/videos';
-@NgModule({
-	declarations: [VideosPipe],
-	imports: [],
-	exports: [VideosPipe]
-})
-export class PipesModule {}
